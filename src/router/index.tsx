fcd09973db56cbe035b9eb4b754365dcc1075986
Jefferson Oliveira <oliveira2112jj@outlook.com>
2025-09-16 16:18:14 -0300

    adjust albums tags on photo widget and config nuqs

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,16 +1,19 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import { LayoutMain } from "../layouts";
 import { Home, PhotoDetails } from "../pages";
+import { NuqsAdapter } from "nuqs/adapters/react-router/v7";
 
 export function Router() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<LayoutMain />}>
-          <Route index element={<Home />} />
-          <Route path="/photos/:id" element={<PhotoDetails />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <NuqsAdapter>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<LayoutMain />}>
+            <Route index element={<Home />} />
+            <Route path="/photos/:id" element={<PhotoDetails />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </NuqsAdapter>
   );
 }
